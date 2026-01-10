4d1f3480ce1be64baeac2a960d46ccee932b9a55
Fadida Junaedy <fadidajunaedy@gmail.com>
2026-01-10 19:22:10 +0700

    feat: adding login command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,27 @@
 declare namespace Cypress {
   interface Chainable {
+    login(email: string, password: string): Chainable<void>;
     addFavoriteArticle(slug: string): Chainable<void>;
     removeFavoriteArticle(slug: string): Chainable<void>;
   }
 }
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.window().then((window) => {
+    cy.request({
+      method: "POST",
+      url: "https://conduit-api.bondaracademy.com/api/users/login",
+      body: {
+        user: {
+          email,
+          password,
+        },
+      },
+    }).then((response) => {
+      window.localStorage.setItem("jwtToken", response.body.user.token);
+    });
+  });
+});
 
 Cypress.Commands.add("addFavoriteArticle", (slug) => {
   cy.window().then((window) => {
