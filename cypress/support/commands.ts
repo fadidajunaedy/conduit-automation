05deed037d4db6f33eaca7680f6d590b1a9b6f94
Fadida Junaedy <fadidajunaedy@gmail.com>
2026-01-10 10:45:07 +0700

    ADDING DELETE FAVORITE ARTICLE COMMAND VIA API

---
+++ cypress/support/commands.ts
@@ -0,0 +1,30 @@
+declare namespace Cypress {
+  interface Chainable {
+    addFavoriteArticle(slug: string): Chainable<void>;
+    removeFavoriteArticle(slug: string): Chainable<void>;
+  }
+}
+
+Cypress.Commands.add("addFavoriteArticle", (slug) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jtwToken");
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env("apiUrl")}/articles/${slug}/favorite`,
+      headers: { Authorization: `Token ${authToken}` },
+      failOnStatusCode: false,
+    });
+  });
+});
+
+Cypress.Commands.add("removeFavoriteArticle", (slug) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jtwToken");
+    cy.request({
+      method: "DELETE",
+      url: `${Cypress.env("apiUrl")}/articles/${slug}/favorite`,
+      headers: { Authorization: `Token ${authToken}` },
+      failOnStatusCode: false,
+    });
+  });
+});
