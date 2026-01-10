c8ac96817cbc7a98a1e460a067b13824d1f3ead2
Fadida Junaedy <fadidajunaedy@gmail.com>
2026-01-10 20:51:05 +0700

    feat: adding case for create article without filling tag

--- cypress/e2e/functional/editor.cy.ts
+++ cypress/e2e/functional/editor.cy.ts
@@ -8,7 +8,7 @@ describe("Editor Page - Positive Cases", () => {
     editorPage.visit();
   });
 
-  it.only("Verify user can publish article with complete valid data", () => {
+  it("Verify user can publish article with complete valid data", () => {
     cy.intercept("POST", "**/articles").as("addArticle");
 
     const nowString = new Date().toString();
@@ -17,6 +17,27 @@ describe("Editor Page - Positive Cases", () => {
     editorPage.fillBody("Lorem ipsum dolor si amet");
     editorPage.fillTags("test");
     editorPage.fillTags("dummy");
+    editorPage.submit();
+
+    cy.wait("@addArticle");
+    cy.url().should("contain", "/article/");
+    cy.get("h1").should("contain", nowString);
+
+    cy.url().then((textUrl) => {
+      const slug = textUrl.split("/article/")[1];
+      cy.removeArticle(slug);
+    });
+  });
+
+  it.only("Verify user can publish article without Tags (Optional field)", () => {
+    cy.intercept("POST", "**/articles").as("addArticle");
+
+    const nowString = new Date().toString();
+    editorPage.fillTitle(`dummy article ${nowString}`);
+    editorPage.fillDescription(`this is dummy article ${nowString}`);
+    editorPage.fillBody("Lorem ipsum dolor si amet");
+    editorPage.fillTags("test");
+    editorPage.removeTags("test");
     editorPage.submit();
 
     cy.wait("@addArticle");
