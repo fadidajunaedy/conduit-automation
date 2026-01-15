b2ad52407f8700339d383e3f42853ef29455bfb1
Fadida Junaedy <fadidajunaedy@gmail.com>
2026-01-15 10:51:10 +0700

    feat: adding command for addCommentArticle, removeCommentArticle, addArticle, and removeArticle

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -7,6 +7,48 @@ declare namespace Cypress {
     password?: string;
   }
 
+  interface CommentResponseBody {
+    comment: {
+      id: number;
+      createdAt: string;
+      updatedAt: string;
+      body: string;
+      author: {
+        username: string;
+        bio: string;
+        image: string;
+        following: boolean;
+      };
+    };
+  }
+
+  interface ArticleRequestBody {
+    title: string;
+    description: string;
+    body: string;
+    tagList: string[];
+  }
+
+  interface ArticleResponseBody {
+    article: {
+      slug: string;
+      title: string;
+      description: string;
+      body: string;
+      tagList: string[];
+      createdAt: string;
+      updatedAt: string;
+      favorited: false;
+      favoritesCount: number;
+      author: {
+        username: string;
+        bio: string;
+        image: string;
+        following: boolean;
+      };
+    };
+  }
+
   interface Chainable {
     login(email: string, password: string): Chainable<void>;
     addFavoriteArticle(slug: string): Chainable<void>;
@@ -17,6 +59,13 @@ declare namespace Cypress {
     unfollowProfile(username: string): Chainable<void>;
     addFavoriteArticle(slug: string): Chainable<void>;
     removeFavoriteArticle(slug: string): Chainable<void>;
+    addCommentArticle(
+      slug: string,
+      body: string
+    ): Chainable<CommentResponseBody>;
+    removeCommentArticle(slug: string, commentId: number): Chainable<void>;
+    addArticle(body: ArticleRequestBody): Chainable<ArticleResponseBody>;
+    removeArticle(slug: string): Chainable<void>;
   }
 }
 
@@ -130,6 +179,66 @@ Cypress.Commands.add("removeFavoriteArticle", (slug) => {
     cy.request({
       method: "DELETE",
       url: `${Cypress.env("apiUrl")}/articles/${slug}/favorite`,
+      headers: { Authorization: `Token ${authToken}` },
+      failOnStatusCode: false,
+    });
+  });
+});
+
+Cypress.Commands.add("addCommentArticle", (slug, body) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jwtToken");
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env("apiUrl")}/articles/${slug}/comments`,
+      headers: { Authorization: `Token ${authToken}` },
+      body: {
+        comment: {
+          body: body,
+        },
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      return response.body;
+    });
+  });
+});
+
+Cypress.Commands.add("removeCommentArticle", (slug, commentId) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jwtToken");
+    cy.request({
+      method: "DELETE",
+      url: `${Cypress.env("apiUrl")}/articles/${slug}/comments/${commentId}`,
+      headers: { Authorization: `Token ${authToken}` },
+      failOnStatusCode: false,
+    });
+  });
+});
+
+Cypress.Commands.add("addArticle", (body) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jwtToken");
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env("apiUrl")}/articles`,
+      headers: { Authorization: `Token ${authToken}` },
+      body: {
+        article: body,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      return response.body;
+    });
+  });
+});
+
+Cypress.Commands.add("removeArticle", (slug) => {
+  cy.window().then((window) => {
+    const authToken = window.localStorage.getItem("jwtToken");
+    cy.request({
+      method: "DELETE",
+      url: `${Cypress.env("apiUrl")}/articles/${slug}`,
       headers: { Authorization: `Token ${authToken}` },
       failOnStatusCode: false,
     });
