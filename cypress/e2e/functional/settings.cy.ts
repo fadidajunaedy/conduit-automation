54e1eb95ecd0cf8cb4598061c05d13cb782768a5
Fadida Junaedy <fadidajunaedy@gmail.com>
2026-01-17 09:04:33 +0700

    ref: changing arrow function into regular function

--- cypress/e2e/functional/settings.cy.ts
+++ cypress/e2e/functional/settings.cy.ts
@@ -2,18 +2,18 @@ import SettingsPage from "../../pages/SettingsPage";
 
 const settingsPage: SettingsPage = new SettingsPage();
 
-describe("Settings Page - Functional Update", () => {
-  beforeEach(() => {
+describe("Settings Page - Functional Update", function () {
+  beforeEach(function () {
     cy.login("fadidajunaedy@mail.com", "qq332211");
     settingsPage.visit();
   });
 
-  it("Verify user can update Profile Picture", () => {
+  it("Verify user can update Profile Picture", function () {
     cy.intercept("PUT", "**/user").as("updateUser");
     cy.intercept("GET", "**/profiles/*").as("getProfileUser");
 
     const newProfileImageUrl = "https://placehold.co/400x400/000000/FFFFFF/png";
-    settingsPage.navbar.profileImageUrl.then((initialUrl) => {
+    settingsPage.navbar.profileImageUrl.then(function (initialUrl) {
       cy.log("initial URL: " + initialUrl);
       cy.log("new URL: " + newProfileImageUrl);
 
@@ -25,7 +25,7 @@ describe("Settings Page - Functional Update", () => {
       cy.url().should("contain", "/profile/");
       cy.get("img[class='user-img']")
         .invoke("attr", "src")
-        .then((src) => {
+        .then(function (src) {
           expect(src).to.be.not.equal(initialUrl);
           expect(src).to.be.equal(newProfileImageUrl);
 
@@ -35,12 +35,12 @@ describe("Settings Page - Functional Update", () => {
     });
   });
 
-  it("Verify user can update Username", () => {
+  it("Verify user can update Username", function () {
     cy.intercept("PUT", "**/user").as("updateUser");
     cy.intercept("GET", "**/profiles/*").as("getProfileUser");
 
     const newUsername = "newfadidajunaedy";
-    settingsPage.navbar.profileUsername.then((text) => {
+    settingsPage.navbar.profileUsername.then(function (text) {
       const initialUsername = text.trim();
       cy.log("Initial Username: " + initialUsername);
       cy.log("New Username: " + newUsername);
@@ -54,7 +54,7 @@ describe("Settings Page - Functional Update", () => {
       cy.get(".profile-page")
         .find("h4")
         .invoke("text")
-        .then((newText) => {
+        .then(function (newText) {
           const appearingNewUsername = newText.trim();
 
           expect(appearingNewUsername).to.be.not.equal(initialUsername);
@@ -66,7 +66,7 @@ describe("Settings Page - Functional Update", () => {
     });
   });
 
-  it("Verify user can update Bio", () => {
+  it("Verify user can update Bio", function () {
     cy.intercept("PUT", "**/user").as("updateUser");
     cy.intercept("GET", "**/profiles/*").as("getProfileUser");
 
@@ -84,7 +84,7 @@ describe("Settings Page - Functional Update", () => {
     cy.get(".profile-page")
       .find("p")
       .invoke("text")
-      .then((newText) => {
+      .then(function (newText) {
         const appearingNewbio = newText.trim();
         expect(appearingNewbio).to.be.not.equal(initialBio);
         expect(appearingNewbio).to.be.equal(newBio);
@@ -95,17 +95,17 @@ describe("Settings Page - Functional Update", () => {
   });
 });
 
-describe("Settings Page - Session Management", () => {
-  beforeEach(() => {
+describe("Settings Page - Session Management", function () {
+  beforeEach(function () {
     cy.login("fadidajunaedy@mail.com", "qq332211");
     settingsPage.visit();
   });
 
-  it("Verify user is redirected to Home page after Logout", () => {
+  it("Verify user is redirected to Home page after Logout", function () {
     settingsPage.logout();
     cy.url().should("equal", "https://conduit.bondaracademy.com/");
 
-    cy.window().then((window) => {
+    cy.window().then(function (window) {
       const authToken = window.localStorage.getItem("jwtToken");
       expect(authToken).to.be.not.exist;
     });
